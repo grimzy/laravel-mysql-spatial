6c426f6abc126b14834f3787758812a5a96c33ab
Joseph Estefane <estefanejoe@gmail.com>
2017-08-11 01:14:46 -0400

    Refactored travis builds to test multiple MySQL versions with Docker

--- start_db.sh
+++ start_db.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-docker run -d --name spatial-mysql \
-    -p 3306:3306 \
+echo "MySQL version: " $MYSQL_VERSION
+
+docker run -d --name spatial-mysql-$MYSQL_VERSION \
+    -p 127.0.0.1:3306:3306 \
     -v $(pwd)/db:/var/lib/mysql \
     -e MYSQL_DATABASE=test \
     -e MYSQL_ALLOW_EMPTY_PASSWORD=yes \
-    mysql:latest
+    mysql:$MYSQL_VERSION --character-set-server=utf8 --collation-server=utf8_general_ci
