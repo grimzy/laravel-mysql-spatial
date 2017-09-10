9a26d66ab1dc69244d0a364e9f1e9f2b9cd1ecc3
Joseph Estefane <estefanejoe@gmail.com>
2017-09-10 12:44:09 -0400

    Added tests for ArrayAccess, prependPoint, appendPoint, insertPoint.

--- tests/Unit/Types/MultiPointTest.php
+++ tests/Unit/Types/MultiPointTest.php
@@ -45,7 +45,71 @@ class MultiPointTest extends BaseTestCase
     }
 
     public function testArrayAccess() {
+        $point0 = new Point(0, 0);
+        $point1 = new Point(1, 1);
+        $multipoint = new MultiPoint([$point0, $point1]);
 
+        $this->assertEquals($point0, $multipoint[0]);
+        $this->assertEquals($point1, $multipoint[1]);
+        $point2 = new Point(2, 2);
+
+        $multipoint[] = $point2;
+        $this->assertEquals($point2, $multipoint[2]);
+
+        unset($multipoint[0]);
+        $this->assertNull($multipoint[0]);
+        $this->assertEquals($point1, $multipoint[1]);
+        $this->assertEquals($point2, $multipoint[2]);
+
+        $point100 = new Point(100, 100);
+        $multipoint[100] = $point100;
+        $this->assertEquals($point100, $multipoint[100]);
+
+        $this->expectException(InvalidArgumentException::class);
+        $multipoint[] = 1;
+
+    }
+
+    public function testPrependPoint() {
+        $point1 = new Point(1, 1);
+        $point2 = new Point(2, 2);
+        $multipoint = new MultiPoint([$point1, $point2]);
+
+        $point0 = new Point(0, 0);
+        $multipoint->prependPoint($point0);
+
+        $this->assertEquals($point0, $multipoint->getPoints()[0]);
+        $this->assertEquals($point1, $multipoint->getPoints()[1]);
+        $this->assertEquals($point2, $multipoint->getPoints()[2]);
+    }
+
+    public function testAppendPoint() {
+        $point0 = new Point(0, 0);
+        $point1 = new Point(1, 1);
+        $multipoint = new MultiPoint([$point0, $point1]);
+
+        $point2 = new Point(2, 2);
+        $multipoint->appendPoint($point2);
+
+        $this->assertEquals($point0, $multipoint->getPoints()[0]);
+        $this->assertEquals($point1, $multipoint->getPoints()[1]);
+        $this->assertEquals($point2, $multipoint->getPoints()[2]);
+    }
+
+    public function testInsertPoint() {
+        $point1 = new Point(1, 1);
+        $point3 = new Point(3, 3);
+        $multipoint = new MultiPoint([$point1, $point3]);
+
+        $point2 = new Point(2, 2);
+        $multipoint->insertPoint(1, $point2);
+
+        $this->assertEquals($point1, $multipoint->getPoints()[0]);
+        $this->assertEquals($point2, $multipoint->getPoints()[1]);
+        $this->assertEquals($point3, $multipoint->getPoints()[2]);
+
+        $this->expectException(InvalidArgumentException::class);
+        $multipoint->insertPoint(100, new Point(100,100));
     }
 
     public function testJsonSerialize()
