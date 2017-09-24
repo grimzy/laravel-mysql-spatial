afebcafce6934fece4c2322e040227f2db62cfd8
Joseph Estefane <estefanejoe@gmail.com>
2017-09-24 14:59:02 -0400

    PointCollection/MultiPoint: Implemented Arrayable and Jsonable and primarily using ArrayAccess for operations (#15).

--- src/Types/PointCollection.php
+++ src/Types/PointCollection.php
@@ -6,11 +6,12 @@ use ArrayAccess;
 use ArrayIterator;
 use Countable;
 use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Jsonable;
 use InvalidArgumentException;
 use IteratorAggregate;
 use JsonSerializable;
 
-abstract class PointCollection implements IteratorAggregate, Arrayable, ArrayAccess, Countable, JsonSerializable
+abstract class PointCollection implements IteratorAggregate, ArrayAccess, Arrayable, Countable, Jsonable, JsonSerializable
 {
     /**
      * @var Point[]
@@ -36,11 +37,6 @@ abstract class PointCollection implements IteratorAggregate, Arrayable, ArrayAcc
         $this->points = $points;
     }
 
-    public function getPoints()
-    {
-        return $this->points;
-    }
-
     public function toArray()
     {
         return $this->points;
@@ -51,35 +47,11 @@ abstract class PointCollection implements IteratorAggregate, Arrayable, ArrayAcc
         return new ArrayIterator($this->points);
     }
 
-    public function prependPoint(Point $point)
-    {
-        array_unshift($this->points, $point);
-    }
-
-    public function appendPoint(Point $point)
-    {
-        $this->points[] = $point;
-    }
-
-    public function insertPoint($index, Point $point)
-    {
-        if (count($this->points) - 1 < $index) {
-            throw new InvalidArgumentException('$index is greater than the size of the array');
-        }
-
-        array_splice($this->points, $index, 0, [$point]);
-    }
-
     public function offsetExists($offset)
     {
         return isset($this->points[$offset]);
     }
 
-    /**
-     * @param mixed $offset
-     *
-     * @return null|Point
-     */
     public function offsetGet($offset)
     {
         return $this->offsetExists($offset) ? $this->points[$offset] : null;
@@ -87,12 +59,12 @@ abstract class PointCollection implements IteratorAggregate, Arrayable, ArrayAcc
 
     public function offsetSet($offset, $value)
     {
-        if (!($value instanceof Point)) {
+        if (! ($value instanceof Point)) {
             throw new InvalidArgumentException('$value must be an instance of Point');
         }
 
         if (is_null($offset)) {
-            $this->appendPoint($value);
+            $this->points[] = $value;
         } else {
             $this->points[$offset] = $value;
         }
@@ -108,10 +80,72 @@ abstract class PointCollection implements IteratorAggregate, Arrayable, ArrayAcc
         return count($this->points);
     }
 
+    public function toJson($options = 0)
+    {
+        return json_encode($this, $options);
+    }
+
     public function toPairList()
     {
         return implode(',', array_map(function (Point $point) {
             return $point->toPair();
         }, $this->points));
+    }
+
+    /**
+     * @return array|\Grimzy\LaravelMysqlSpatial\Types\Point[]
+     *
+     * @deprecated 2.1.0 Use $multipoint->toArray() instead
+     *
+     * @see IteratorAggregate
+     * @see ArrayAccess
+     * @see Arrayable
+     */
+    public function getPoints()
+    {
+        return $this->points;
+    }
+
+    /**
+     * @param \Grimzy\LaravelMysqlSpatial\Types\Point $point
+     *
+     * @deprecated 2.1.0 Use array_unshift($multipoint, $point); instead
+     *
+     * @see array_unshift
+     * @see ArrayAccess
+     */
+    public function prependPoint(Point $point)
+    {
+        array_unshift($this->points, $point);
+    }
+
+    /**
+     * @param \Grimzy\LaravelMysqlSpatial\Types\Point $point
+     *
+     * @deprecated 2.1.0 Use $multipoint[] = $point; instead
+     *
+     * @see ArrayAccess
+     */
+    public function appendPoint(Point $point)
+    {
+        $this->points[] = $point;
+    }
+
+    /**
+     * @param $index
+     * @param \Grimzy\LaravelMysqlSpatial\Types\Point $point
+     *
+     * @deprecated 2.1.0 Use array_splice($multipoint, $index, 0, [$point]); instead
+     *
+     * @see array_splice
+     * @see ArrayAccess
+     */
+    public function insertPoint($index, Point $point)
+    {
+        if (count($this->points) - 1 < $index) {
+            throw new InvalidArgumentException('$index is greater than the size of the array');
+        }
+
+        array_splice($this->points, $index, 0, [$point]);
     }
 }
