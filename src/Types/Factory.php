0105b9894a8dc984d6187c59c17e5f0feb4ccd4c
nazaninhesari <32956660+nazaninhesari@users.noreply.github.com>
2019-09-28 09:24:18 +0330

    flip y,x change store in db lng,lat

--- src/Types/Factory.php
+++ src/Types/Factory.php
@@ -6,7 +6,7 @@ class Factory implements \GeoIO\Factory
 {
     public function createPoint($dimension, array $coordinates, $srid = null)
     {
-        return new Point($coordinates['y'], $coordinates['x']);
+        return new Point($coordinates['x'], $coordinates['y']);
     }
 
     public function createLineString($dimension, array $points, $srid = null)
