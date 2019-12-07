7d0578b17c92f12e35feffb41c70aac5f055e207
Saif M <sikhlana@gmail.com>
2019-12-07 13:10:33 +0600

    Fixes tests.

--- tests/Integration/SridSpatialTest.php
+++ tests/Integration/SridSpatialTest.php
@@ -11,7 +11,7 @@ class SridSpatialTest extends IntegrationBaseTestCase
 {
     protected $migrations = [
         CreateLocationTable::class,
-        UpdateLocationTable::class
+        UpdateLocationTable::class,
     ];
 
     public function testInsertPointWithSrid()
