35f8031af220b79306e5e09744956a322f3c7877
Joseph Estefane <estefanejoe@gmail.com>
2020-03-03 00:46:01 -0500

    Last Oops

--- tests/Unit/BaseTestCase.php
+++ tests/Unit/BaseTestCase.php
@@ -7,7 +7,7 @@ abstract class BaseTestCase extends PHPUnit_Framework_TestCase
         Mockery::close();
     }
 
-    protected function assertException($exceptionName, $exceptionMessage = 'uoioi', $exceptionCode = 0)
+    protected function assertException($exceptionName, $exceptionMessage = '', $exceptionCode = 0)
     {
         if (method_exists(parent::class, 'expectException')) {
             parent::expectException($exceptionName);
